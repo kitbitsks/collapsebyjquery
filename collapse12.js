63a968e130b03ca5ec3c253f43375fa55c194650
souravkitkat2706 <souravsirvastava027@gmail.com>
2018-03-31 22:33:56 +0530

    initialized

---
+++ collapse12.js
@@ -0,0 +1,11 @@
+$(document).ready(()=>{
+	$("#gp1").click(()=>{
+		$("#gp").toggle("slow")
+	})
+	$("#gp2").click(()=>{
+		$("#gpp").toggle("slow")
+	})
+	$("#gp3").click(()=>{
+		$("#gppp").toggle("slow")
+	})
+})
